01d1d24f07665c7b4c149a9ed9f46948617c35f6
Эдуард Мединский <eduard.medinschii@gmail.com>
2019-03-28 12:02:54 +0200

    update

--- waaaar/script.js
+++ waaaar/script.js
@@ -13,69 +13,94 @@ var rows;
 var resolution = 10;
 var cells;
 
-function drawCell(x,y,color){
+var gameStarted = false;
+function startGame(){
+  gameStarted = true;
+}
+function save(){
+  var colorOption = document.getElementById("color");
+  var tempratureOption = document.getElementById("temprature");
+  
+}
+var counter = {
+  green: 0,
+  blue: 0,
+  red: 0
+}
+
+function drawCell(x, y, color) {
   push();
-  fill(color == "RED" ? 255 : 0,color == "GREEN" ? 255 : 0,color == "BLUE" ? 255 : 0);
-  rect(x,y,resolution,resolution);
+  fill(color == "RED" ? 255 : 0, color == "GREEN" ? 255 : 0, color == "BLUE" ? 255 : 0);
+  rect(x, y, resolution, resolution);
   pop();
 }
 
-class Cell{
+class Cell {
   health = 100;
   color = "NONE"
   isAlive = false;
-  lastHealth = 100;
   dmg = 1;
-  getDamage(){
-    if(this.color == "RED" && tempratureOption[tempratureOption.selectedIndex].value == "HOT"){  this.dmg = 0.3; }
-    if(this.color == "RED" && tempratureOption[tempratureOption.selectedIndex].value == "WARM"){  this.dmg = 0.6; }
-    if(this.color == "RED" && tempratureOption[tempratureOption.selectedIndex].value == "COLD"){  this.dmg = 0.9; }
+  startDmgMultiplier = 1;
+  dmgMultiplier = 1;
 
-    if(this.color == "BLUE" && tempratureOption[tempratureOption.selectedIndex].value == "HOT"){  this.dmg = 0.9; }
-    if(this.color == "BLUE" && tempratureOption[tempratureOption.selectedIndex].value == "WARM"){  this.dmg = 0.6; }
-    if(this.color == "BLUE" && tempratureOption[tempratureOption.selectedIndex].value == "COLD"){  this.dmg = 0.3; }
+  getDamage() {
+    if (this.color == "RED" && tempratureOption[tempratureOption.selectedIndex].value == "HOT") { this.dmgMultiplier = 3; }
+    if (this.color == "RED" && tempratureOption[tempratureOption.selectedIndex].value == "WARM") { this.dmgMultiplier = 2; }
+    if (this.color == "RED" && tempratureOption[tempratureOption.selectedIndex].value == "COLD") { this.dmgMultiplier = 1; }
 
-    if(this.color == "GREEN" && tempratureOption[tempratureOption.selectedIndex].value == "HOT"){  this.dmg = 0.6; }
-    if(this.color == "GREEN" && tempratureOption[tempratureOption.selectedIndex].value == "WARM"){  this.dmg = 0.3; }
-    if(this.color == "GREEN" && tempratureOption[tempratureOption.selectedIndex].value == "COLD"){  this.dmg = 0.6; }
+    if (this.color == "BLUE" && tempratureOption[tempratureOption.selectedIndex].value == "HOT") { this.dmgMultiplier = 1; }
+    if (this.color == "BLUE" && tempratureOption[tempratureOption.selectedIndex].value == "WARM") { this.dmgMultiplier = 2; }
+    if (this.color == "BLUE" && tempratureOption[tempratureOption.selectedIndex].value == "COLD") { this.dmgMultiplier = 3; }
+
+    if (this.color == "GREEN" && tempratureOption[tempratureOption.selectedIndex].value == "HOT") { this.dmgMultiplier = 1; }
+    if (this.color == "GREEN" && tempratureOption[tempratureOption.selectedIndex].value == "WARM") { this.dmgMultiplier = 2; }
+    if (this.color == "GREEN" && tempratureOption[tempratureOption.selectedIndex].value == "COLD") { this.dmgMultiplier = 1; }
 
     this.health -= this.dmg;
-      
   }
 
-  createNew(health,color){
+  createNew(health, color) {
     this.color = color;
     this.health = health;
     this.isAlive = true;
   }
 
-  update(){
-    if(this.health <= 0){
-        this.isAlive = false;
-        return false;
+  update() {
+    if (this.health <= 0) {
+      this.isAlive = false;
+      return false;
     }
 
-    if(this.isAlive){
+    if (this.isAlive) {
       this.getDamage();
-      if(Math.floor(this.health) % EDGE_FOR_MULTIPLYING == 0){
-        if(Math.floor(this.health) == this.lastHealth){
-          return false;
-        }
-        console.log(this.health + "  " + this.lastHealth) 
+      if (this.health % EDGE_FOR_MULTIPLYING == 0) {
         return true
       }
 
     }
-    this.lastHealth = this.health;
     return false;
   }
 
-  absorbCell(cell){
-    this.health += this.color == cell.color ? cell.health / 2 : -cell.health;
+  absorbCell(cell) {
+    let amountOfCells;
+    switch (cell.color) {
+      case "GREEN":
+        amountOfCells = counter.green;
+        break;
+      case "BLUE":
+        amountOfCells = counter.blue;
+        break;
+      case "RED":
+        amountOfCells = counter.red;
+        break;
+    }
+
+    this.health += this.color == cell.color ? cell.health : -cell.health * cell.dmgMultiplier * (cells.length * cells[0].length / amountOfCells);
+
   }
 
-  draw(i,j){
-    if(this.isAlive){ drawCell(i * resolution,j * resolution,this.color); }
+  draw(i, j) {
+    if (this.isAlive) { drawCell(i * resolution, j * resolution, this.color); }
   }
 }
 
@@ -88,78 +113,85 @@ function setup() {
   initCells();
 }
 
-
-
 function draw() {
   background(155);
   drawGrid();
-  drawAndUpdateCells();
-}
-
-
-function countNeighbors(grid,x,y) {
-
-}
-
-function drawGrid(){
-  for(let i = 0; i < rows; i++){
-    line(0,i * resolution ,width,i * resolution);  
-  }
-
-  for(let i = 0; i < cols; i++){
-    line(i * resolution, 0 ,i * resolution,height);  
+  if(gameStarted){
+    drawAndUpdateCells();
   }
 }
-function getSelctedColor(){
+
+function drawGrid() {
+  for (let i = 0; i < rows; i++) {
+    line(0, i * resolution, width, i * resolution);
+  }
+
+  for (let i = 0; i < cols; i++) {
+    line(i * resolution, 0, i * resolution, height);
+  }
+}
+function getSelctedColor() {
   return colorOption[colorOption.selectedIndex].value;
 }
-function mouseClicked(){
+function mouseClicked() {
   let i = Math.floor(mouseX / resolution);
   let j = Math.floor(mouseY / resolution);
-  if(mouseX < width && mouseY < height && mouseX >=0 && mouseY >= 0){
-    cells[i][j].color = getSelctedColor();  
-    cells[i][j].isAlive = true;  
+  if (mouseX < width && mouseY < height && mouseX >= 0 && mouseY >= 0) {
+    cells[i][j].color = getSelctedColor();
+    cells[i][j].isAlive = true;
   }
 }
 
-function initCells(){  
+function initCells() {
   cells = new Array(cols);
-  for(let i = 0; i < cells.length ; i++){
+  for (let i = 0; i < cells.length; i++) {
     cells[i] = new Array(rows);
   }
 
-  for(let i = 0 ; i < cells.length ; i++){
-    for(let j = 0 ; j < cells[i].length ; j++){
+  for (let i = 0; i < cells.length; i++) {
+    for (let j = 0; j < cells[i].length; j++) {
       cells[i][j] = new Cell();
     }
   }
 }
 
-function drawAndUpdateCells(){
-  for(let i = 0 ; i < cells.length ; i++){
-    for(let j = 0 ; j < cells[i].length ; j++){
-      if(cells[i][j].update()){
-        createInAround(i,j);
+function drawAndUpdateCells() {
+  for (let i = 0; i < cells.length; i++) {
+    for (let j = 0; j < cells[i].length; j++) {
+      switch (cells[i][j].color) {
+        case "GREEN":
+          counter.green++;
+          break;
+        case "BLUE":
+          counter.blue++;
+          break;
+        case "RED":
+          counter.red++;
+          break;
       }
-      cells[i][j].draw(i,j);
+
+      if (cells[i][j].update()) {
+        createInAround(i, j);
+      }
+      cells[i][j].draw(i, j);
     }
   }
 }
 
-function createInAround(i,j){
+function createInAround(i, j) {
   let indexI = i + Math.floor(Math.random() * 3) - 1;
   let indexJ = j + Math.floor(Math.random() * 3) - 1;
-  
-  if(indexI == -1) { indexI = Math.floor(Math.random() * 2); }
-  if(indexI == cells.length - 1) { indexI =  i + Math.floor(Math.random() * 2) ; }
-  if(indexJ == -1) { indexI = Math.floor(Math.random() * 2); }
-  if(indexJ == cells[0].length - 1) { indexJ =  j + Math.floor(Math.random() * 2) ; }
 
-  if(i > 0 && j > 0 && i < cells.length - 1 && j < cells[0].length - 1){
-    if(cells[indexI][indexJ].isAlive){
+  if (indexI == -1) { indexI = Math.floor(Math.random() * 2); }
+  if (indexI == cells.length - 1) { indexI = i + Math.floor(Math.random() * 2); }
+  if (indexJ == -1) { indexI = Math.floor(Math.random() * 2); }
+  if (indexJ == cells[0].length - 1) { indexJ = j + Math.floor(Math.random() * 2); }
+
+  if (i > 0 && j > 0 && i < cells.length - 1 && j < cells[0].length - 1) {
+    if (cells[indexI][indexJ].isAlive) {
       cells[indexI][indexJ].absorbCell(cells[i][j]);
-    }else{
-      cells[indexI][indexJ].createNew(100,cells[i][j].color);
+    } else {
+      cells[indexI][indexJ].createNew(100, cells[i][j].color);
     }
-  } 
+  }
 }
